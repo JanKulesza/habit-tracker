97248f3adc867c702dced96f6b1050704152adae
JanKulesza <jan.kulesza87@gmail.com>
2026-06-20 14:23:10 +0200

    Add: Authentication Backend

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,21 @@
-import { betterAuth } from "better-auth";
+import { betterAuth } from "better-auth/minimal";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+
+if(!process.env["AUTH_GOOGLE_ID"] || ! process.env["AUTH_GOOGLE_SECRET"])
+    throw new Error("Auth enviromental variables are not defined.");
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", 
     }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env["AUTH_GOOGLE_ID"],
+            clientSecret: process.env["AUTH_GOOGLE_SECRET"]
+        }
+    }
 });
