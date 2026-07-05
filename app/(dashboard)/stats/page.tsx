a084781a97b490797a131143ea6171bd4c4ea0a8
JanKulesza <jan.kulesza87@gmail.com>
2026-07-05 20:02:09 +0200

    Imporve charts style with card

--- app/(dashboard)/stats/page.tsx
+++ app/(dashboard)/stats/page.tsx
@@ -1,13 +1,14 @@
 import CustomAreaChart from "@/components/charts/area-chart";
 import CustomRadarChart from "@/components/charts/radar-chart";
 import InfoBox from "@/components/info-box";
-import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { ChartConfig } from "@/components/ui/chart";
 import { getEntriesForCurrentUser } from "@/lib/dal/entries";
 import { getHabitsForCurrentUser } from "@/lib/dal/habits";
 import { completionRatePerRange } from "@/lib/utils";
-import { subDays, startOfDay, format, subMonths } from "date-fns";
+import { subDays, startOfDay, format } from "date-fns";
+import { sort } from "fast-sort";
 import { TrendingUp, Flame, Medal, Check } from "lucide-react";
-import { CartesianGrid, XAxis, Area, AreaChart } from "recharts";
 import { toast } from "sonner";
 
 export default async function StatsPage() {
@@ -59,7 +60,7 @@ export default async function StatsPage() {
     const areaChartData = (() => {
         let data: { date: string, completion: number }[] = [];
 
-        for (let i = 12; i >= 1; i--)
+        for (let i = 12; i >= 1; i--) // Keep in sync with the chart's footer description
             data.push({
                 date: format(subDays(startOfDay(date), 7 * i), "dd LLL"),
                 completion: completionRatePerRange(subDays(date, 7 * i), 7, entries, habits.length)
@@ -76,18 +77,18 @@ export default async function StatsPage() {
     } satisfies ChartConfig
 
     const radarChartData = (() => {
-        let data: { icon: string, completion: number }[] = [];
+        let data: { name: string, completion: number }[] = [];
 
-        for(const val of habits)
-                data.push({
-                icon: val.icon,
+        for (const val of habits)
+            data.push({
+                name: `${val.icon} ${val.name}`,
                 completion: completionRatePerRange(subDays(date, 7), 7, entries.filter(e => e.habitId === val.id), 1)
             })
 
         return data;
     })()
 
-    const radarChartConfig = { 
+    const radarChartConfig = {
         completion: {
             label: "Completion %",
             color: "var(--primary)"
@@ -113,20 +114,52 @@ export default async function StatsPage() {
                 )}
             </div>
             <div className="flex flex-col lg:flex-row gap-4 w-full">
-                <div className="lg:w-2/3 space-y-8 border rounded-lg p-6">
-                    <div className="">
-                        <h2 className="font-medium">Week trend</h2>
-                        <p className="text-muted-foreground text-sm">Completion percent in recent weeks</p>
-                    </div>
-                    <CustomAreaChart chartConfig={areaChartConfig} chartData={areaChartData} dataKeyChart="completion" dataKeyXAxis="date" className="w-full max-h-75! max-sm:h-64!" />
-                </div>
-                <div className="lg:w-1/3 space-y-8 border rounded-lg p-6">
-                    <div className="">
-                        <h2 className="font-medium">Completion Rate per Habit</h2>
-                        <p className="text-muted-foreground text-sm">Completion percent per each habit this month</p>
-                    </div>
-                    <CustomRadarChart chartConfig={radarChartConfig} chartData={radarChartData} dataKeyChart="completion" labels="icon" className="mx-auto aspect-square max-h-62.5!" />
-                </div>
+                <Card className="lg:w-2/3 pt-0">
+                    <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
+                        <div className="grid flex-1 gap-1">
+                            <CardTitle>Week trend</CardTitle>
+                            <CardDescription>
+                                Completion percent in the last 12 weeks
+                            </CardDescription>
+                        </div>
+                    </CardHeader>
+                    <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+                        <CustomAreaChart chartConfig={areaChartConfig} chartData={areaChartData} dataKeyChart="completion" dataKeyXAxis="date" className="w-full max-h-75! max-sm:h-64!" />
+                    </CardContent>
+                    <CardFooter className="h-24">
+                        <div className="grid gap-2">
+                            <div className="flex items-center gap-2 leading-none font-medium">
+                                Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+                            </div>
+                            <div className="flex items-center gap-2 leading-none text-muted-foreground">
+                                {format(subDays(date, 7 * 12), "dd LLL yyyy")} - {format(date, "dd LLL yyyy")}
+                            </div>
+                        </div>
+                    </CardFooter>
+                </Card>
+                <Card className="lg:w-1/3 pt-0 justify-between">
+                    <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
+                        <div className="grid flex-1 gap-1">
+                            <CardTitle>Completion Rate per Habit</CardTitle>
+                            <CardDescription>
+                                Completion percent per each habit this month
+                            </CardDescription>
+                        </div>
+                    </CardHeader>
+                    <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+                        <CustomRadarChart chartConfig={radarChartConfig} chartData={radarChartData} dataKeyChart="completion" labels="name" className="mx-auto w-full max-h-62.5!" />
+                    </CardContent>
+                    <CardFooter className="h-24">
+                        <div className="grid gap-2">
+                            <p>
+                                <span className="font-semibold text-foreground">{sort(radarChartData).desc((d) => d.completion)[0].name}</span> has the highest completion rate this month
+                            </p>
+                            <div className="text-muted-foreground">
+                                {format(date, "LLL yyyy")}
+                            </div>
+                        </div>
+                    </CardFooter>
+                </Card>
             </div>
         </>
     )
